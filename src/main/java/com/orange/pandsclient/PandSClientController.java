7ee31a261e3e6ee04d7ac039b4744433c4299db7
Shyamkumar Chintakindi <SC0063270@Techmahindra.com>
2019-04-25 18:21:28 +0200

    Value mapping with TYPo

--- src/main/java/com/orange/pandsclient/PandSClientController.java
+++ src/main/java/com/orange/pandsclient/PandSClientController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-mport org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Collection;
 
